41465fc31ff390892f2ddbc9594afa8ce217a98e
Kiryl Klimovich <kirylklimovich@gmail.com>
2019-07-10 00:45:50 +0300

    Change google maps to yandex maps

--- app/javascript/components/welcome/index.jsx
+++ app/javascript/components/welcome/index.jsx
@@ -1,47 +1,71 @@
+// TODO Add yandex api to maps
 import React from 'react';
 import MainNavbar from 'components/navbar/navbar'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import I18n from 'i18n-js/index.js.erb'
+import {
+  YMaps,
+  Map,
+  Placemark,
+  ZoomControl,
+  FullscreenControl,
+  GeolocationControl,
+  ListBox,
+  ListBoxItem,
+  RouteButton,
+  RouteEditor, RoutePanel, RulerControl, SearchControl, TypeSelector
+} from "react-yandex-maps";
 
-const style = {
-  width: '60%',
-  height: '85%',
-  marginLeft: '4rem'
+const mapData = {
+  center: [53.9045, 27.5615],
+  zoom: 5,
+  controls: []
 };
 
-const minskCoordinates = {
-  lat: 53.9045,
-  lng: 27.5615
-};
+const coordinates = [
+  [53.9045, 27.5615]
+];
 
 class WelcomePage extends React.Component {
     render () {
         return (
             <div>
-                <MainNavbar></MainNavbar>
-                <div className="jumbotron">
-                    <h1>{I18n.t('welcome_page.organization_name')}</h1>
-                    <p>{I18n.t('welcome_page.moto')}</p>
-                </div>
+              <MainNavbar></MainNavbar>
+              <div className="jumbotron">
+                  <h1>{I18n.t('welcome_page.organization_name')}</h1>
+                  <p>{I18n.t('welcome_page.moto')}</p>
+              </div>
 
-                <div className='container'>
-                  <Map google={this.props.google}
-                       zoom={14}
-                       style={style}
-                       initialCenter={minskCoordinates}
-                  >
-                    <Marker onClick={this.onMarkerClick}
-                            name={'Current location'} />
-                    <InfoWindow onClose={this.onInfoWindowClose}>
-                    </InfoWindow>
-                  </Map>
-                </div>
+              <div>
+                <YMaps>
+                  <div id='map' className='map'>
+                    <Map defaultState={mapData} width='100%' height='100%'>
+                      <ZoomControl options={{ float: 'right' }} />
+                      <FullscreenControl />
+                      <RouteEditor />
+                      <GeolocationControl options={{ float: 'left' }} />
+                      <ListBox data={{ content: 'Select city' }}>
+                        <ListBoxItem data={{ content: 'Moscow' }} />
+                        <ListBoxItem
+                          data={{
+                            content: 'Saint Petersburg',
+                          }}
+                        />
+                      </ListBox>
+                      <RouteButton options={{ float: 'right' }} />
+                      <RoutePanel options={{ float: 'right' }} />
+                      <RulerControl options={{ float: 'right' }} />
+                      <SearchControl options={{ float: 'right' }} />
+                      <TypeSelector options={{ float: 'right' }} />
+
+                      {coordinates.map(coordinate => <Placemark geometry={coordinate} />)}
+                    </Map>
+                  </div>
+                </YMaps>
+              </div>
             </div>
 
         );
     }
 }
 
-export default GoogleApiWrapper({
-  apiKey: (process.env.GOOGLE_MAPS_API_KEY)
-})(WelcomePage)
+export default WelcomePage;
