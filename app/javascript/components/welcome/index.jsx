32bd8d45ba12729148e5326d14af5c822bb57034
Kiryl Klimovich <kirylklimovich@gmail.com>
2019-07-09 19:28:29 +0300

    Add google map on main page

--- app/javascript/components/welcome/index.jsx
+++ app/javascript/components/welcome/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import MainNavbar from 'components/navbar/navbar'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+const style = {
+  width: '60%',
+  height: '85%',
+  marginLeft: '4rem'
+};
+
+const minskCoordinates = {
+  lat: 53.9045,
+  lng: 27.5615
+};
 
 class WelcomePage extends React.Component {
     render () {
@@ -10,10 +22,25 @@ class WelcomePage extends React.Component {
                     <h1>Green Minsk</h1>
                     <p>Care about our planet</p>
                 </div>
+
+                <div className='container'>
+                  <Map google={this.props.google}
+                       zoom={14}
+                       style={style}
+                       initialCenter={minskCoordinates}
+                  >
+                    <Marker onClick={this.onMarkerClick}
+                            name={'Current location'} />
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                    </InfoWindow>
+                  </Map>
+                </div>
             </div>
 
         );
     }
 }
 
-export default WelcomePage
+export default GoogleApiWrapper({
+  apiKey: (process.env.GOOGLE_MAPS_API_KEY)
+})(WelcomePage)
